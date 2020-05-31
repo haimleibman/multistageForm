13bf219015166ad031f56000443705fd6fd5fc8f
חיים לייבמן <hnl770@gmail.com>
2020-05-31 14:25:55 +0300

    dialig fix with portals

--- src/SuccesModal/SuccessModal.jsx
+++ src/SuccesModal/SuccessModal.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './SuccessModal.module.css';
 import useLanguageSwitcher from '../sharedModels/LanguageSwitcher';
+import { createPortal } from 'react-dom';
 
 const SuccessModal = (props) => {
     const SuccessMessage = useLanguageSwitcher('The information was successfully absorbed!')
 
-    return (
+    return createPortal(
         <>
             {props.isOpen && <div className={styles.background} onClick={() => props.closeDialog()}/>}
             <dialog className={styles.success} open={props.isOpen}>{SuccessMessage}</dialog>
-        </>
+            </>,
+            document.getElementsByTagName('body')[0]
     )
 }
 
